d964e80bb4c8a6d0ae186220822510bfd7f557e5
yuvakrishnayk <165897286+yuvakrishnayk@users.noreply.github.com>
2026-01-31 22:45:57 +0530

    feat: integrate dotenv for environment variable management and update API key handling

--- explainer_agent/agent.py
+++ explainer_agent/agent.py
@@ -1,11 +1,14 @@
 from crewai import Agent
 from explainer_agent.tools import FirestoreQueryTool
 from crewai import LLM
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 llm = LLM(
     model="gemini/gemini-2.5-flash",
-    api_key="AIzaSyB1Cyr0XD5kthRclnJaKgpoKAR0NzXpyVM",
+    api_key=os.getenv("GOOGLE_API_KEY"),
     temperature=0.2
 )
 
